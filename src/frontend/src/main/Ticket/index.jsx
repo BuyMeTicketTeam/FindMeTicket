5751f654f0b9676942b8fdc31ae6f405c856993b
Stepan Stadniuk <109167846+Stepan22-prog@users.noreply.github.com>
2023-12-21 10:00:13 +0200

    Added display of the full name of the station when hovering.

--- src/frontend/src/main/Ticket/index.jsx
+++ src/frontend/src/main/Ticket/index.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import './ticket.css';
 
 export default function Ticket({ data }) {
-  const placeFrom = data.placeFrom.length > 27 ? `${data.placeFrom.slice(0, 27)}...` : data.placeFrom;
-  const placeAt = data.placeAt.length > 27 ? `${data.placeAt.slice(0, 27)}...` : data.placeAt;
+  const placeFrom = data.placeFrom.length > 27 ? `${data.placeFrom.slice(0, 25)}...` : data.placeFrom;
+  const placeAt = data.placeAt.length > 27 ? `${data.placeAt.slice(0, 25)}...` : data.placeAt;
+  const placeFromTitle = data.placeFrom.length > 27 ? data.placeFrom : null;
+  const placeAtTitle = data.placeAt.length > 27 ? data.placeAt : null;
+  const { t } = useTranslation('translation', { keyPrefix: 'ticket' });
   return (
     <div className="ticket">
       <div className="ticket__body">
@@ -22,17 +26,17 @@ export default function Ticket({ data }) {
             <p className="ticket__city ticket-last-element">{data.arrivalCity}</p>
           </div>
           <div className="information__row">
-            <div className="ticket__station">{placeFrom}</div>
-            <div className="ticket__station ticket-last-element">{placeAt}</div>
+            <div className="ticket__station" title={placeFromTitle}>{placeFrom}</div>
+            <div className="ticket__station ticket-last-element" title={placeAtTitle}>{placeAt}</div>
           </div>
         </div>
         <div className="price-options">
           <div className="price-options__row">
             <div className="ticket__price">
-              <p className="price__ordinry">{`${data.price} грн`}</p>
-              <p className="price__discond">{data.priceOld ? `${data.priceOld} грн` : null}</p>
+              <p className="price__ordinry">{`${data.price} ${t('uan')}`}</p>
+              <p className="price__discond">{data.priceOld ? `${data.priceOld} ${t('uan')}` : null}</p>
             </div>
-            <a className="ticket__buy button" href={data.url} name="Купити" target="blank">Обрати</a>
+            <a className="ticket__buy button" href={data.url} target="blank">{t('select')}</a>
           </div>
         </div>
       </div>
