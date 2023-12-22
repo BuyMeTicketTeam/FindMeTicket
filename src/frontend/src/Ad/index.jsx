cebea980792fa6abf8cd5ca68f3f173da59fa05b
Stepan Stadniuk <109167846+Stepan22-prog@users.noreply.github.com>
2023-12-22 19:46:55 +0200

    Added locaization

--- src/frontend/src/Ad/index.jsx
+++ src/frontend/src/Ad/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import './ad.css';
 
 export default function Index({ isBig }) {
+  const { t } = useTranslation('translation', { keyPrefix: 'ad' });
   return (
     <div className={`ad-container ${isBig ? 'ad_big' : ''}`}>
-      <div className="ad-text">Тут може бути ваша реклама!</div>
+      <div className="ad-text">{t('title')}</div>
     </div>
   );
 }
--- src/frontend/src/main/Error/index.jsx
+++ src/frontend/src/main/Error/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import './error.css';
 
 export default function Error() {
+  const { t } = useTranslation('translation', { keyPrefix: 'main-error' });
   return (
     <div className="main-error">
       <img className="main-error__img" src="../img/error.svg" alt="Error" />
-      <h2 className="main-error__title">Виникла помилка</h2>
-      <p className="main-error__text">Просимо вибачення за незручності, спробуйте ще раз</p>
+      <h2 className="main-error__title">{t('title')}</h2>
+      <p className="main-error__text">{t('text')}</p>
     </div>
   );
 }
