c175e6eeb444ed5f4510b7ee392b34db36cb0b3a
Stepan Stadniuk <109167846+Stepan22-prog@users.noreply.github.com>
2023-12-26 16:02:07 +0200

    Added tests to components such as passanger, ticket, tourists, transport

--- src/frontend/src/main/Ticket/ticket.test.js
+++ src/frontend/src/main/Ticket/ticket.test.js
@@ -1,33 +1,67 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import Ticket from '.';
+import Ticket from './index';
 
 const mockData = {
-  dateFrom: '2023-01-01',
+  departureTime: '12:00',
+  departureDate: '2023-01-01',
   travelTime: '2h 30m',
-  dateArrival: '2023-01-02',
-  placeFrom: 'City A',
-  placeTo: 'City B',
-  placeFromDetails: 'Station A',
-  placeToDetails: 'Station B',
-  priceOrdinary: 100,
+  arrivalTime: '14:30',
+  arrivalDate: '2023-01-01',
+  departureCity: 'City A',
+  arrivalCity: 'City B',
+  placeFrom: 'Station A',
+  placeAt: 'Station B',
+  price: 100,
   priceOld: 120,
-  tickerCarrier: 'nikkaBus',
+  url: 'https://example.com',
 };
 
-test('Ticket component renders with correct data', () => {
+test('renders ticket component with correct data', () => {
   render(<Ticket data={mockData} />);
 
-  expect(screen.getByText(mockData.dateFrom)).toBeInTheDocument();
-  expect(screen.getByText(mockData.travelTime)).toBeInTheDocument();
-  expect(screen.getByText(mockData.dateArrival)).toBeInTheDocument();
-  expect(screen.getByText(mockData.placeFrom)).toBeInTheDocument();
-  expect(screen.getByText(mockData.placeTo)).toBeInTheDocument();
-  expect(screen.getByText(mockData.placeFromDetails)).toBeInTheDocument();
-  expect(screen.getByText(mockData.placeToDetails)).toBeInTheDocument();
-  expect(screen.getByText(`${mockData.priceOrdinary} грн`)).toBeInTheDocument();
-  expect(screen.getByText(`${mockData.priceOld} грн`)).toBeInTheDocument();
-  expect(screen.getByText('Купити')).toBeInTheDocument();
-  expect(screen.getByText(`Перевізник: ${mockData.tickerCarrier}`)).toBeInTheDocument();
+  // Check if the ticket information is rendered correctly
+  expect(screen.getByText('12:00 | 2023-01-01')).toBeInTheDocument();
+  expect(screen.getByText('2h 30m')).toBeInTheDocument();
+  expect(screen.getByText('14:30 | 2023-01-01')).toBeInTheDocument();
+  expect(screen.getByText('City A')).toBeInTheDocument();
+  expect(screen.getByText('City B')).toBeInTheDocument();
+  expect(screen.getByText('Station A')).toBeInTheDocument();
+  expect(screen.getByText('Station B')).toBeInTheDocument();
+
+  // Check if the price information is rendered correctly
+  expect(screen.getByText('100 uan')).toBeInTheDocument();
+  expect(screen.getByText('120 uan')).toBeInTheDocument(); // Check for discounted price
+  expect(screen.getByText('select')).toBeInTheDocument();
+
+  // Check if the link has the correct href
+  const link = screen.getByText('select');
+  expect(link).toHaveAttribute('href', 'https://example.com');
+});
+
+test('truncates long station names', () => {
+  const longStationData = {
+    ...mockData,
+    placeFrom: 'This is a very long station name that needs to be truncated',
+    placeAt: 'Another very long station name that needs to be truncated',
+  };
+
+  render(<Ticket data={longStationData} />);
+
+  // Check if long station names are truncated
+  expect(screen.getByText('This is a very long stati...')).toBeInTheDocument();
+  expect(screen.getByText('Another very long station...')).toBeInTheDocument();
+});
+
+test('does not display old price if it is not available', () => {
+  const dataWithoutOldPrice = {
+    ...mockData,
+    priceOld: null,
+  };
+
+  render(<Ticket data={dataWithoutOldPrice} />);
+
+  // Check if the old price is not displayed
+  expect(screen.queryByText('120 uan')).not.toBeInTheDocument();
 });
