01961f77cf786ea5f5a6734ad6ed2369049be8b7
Mykhailo Marchuk <mishaakamichael999@gmail.com>
2024-02-01 19:28:05 +0200

    Refactoring mapper to parse date json without leading zeros

--- src/main/java/com/booking/app/mapper/TicketMapper.java
+++ src/main/java/com/booking/app/mapper/TicketMapper.java
@@ -1,7 +1,6 @@
 package com.booking.app.mapper;
 
 import com.booking.app.dto.TicketDTO;
-import com.booking.app.entity.Route;
 import com.booking.app.entity.Ticket;
 import org.mapstruct.*;
 
@@ -12,18 +11,17 @@ import java.util.Locale;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
 public interface TicketMapper {
+
     Ticket toEntity(TicketDTO ticketDTO);
 
-
-    @Mapping(source ="route.departureCity" ,target ="departureCity")
-    @Mapping(source ="route.arrivalCity" ,target ="arrivalCity")
-    @Mapping(source ="route.departureDate" ,target ="departureDate", qualifiedByName = "departureTimeMapping")
-    @Mapping(source ="travelTime" ,target ="travelTime", qualifiedByName = "decimalToString")
+    @Mapping(source = "route.departureCity", target = "departureCity")
+    @Mapping(source = "route.arrivalCity", target = "arrivalCity")
+    @Mapping(source = "route.departureDate", target = "departureDate", qualifiedByName = "departureTimeMapping")
+    @Mapping(source = "travelTime", target = "travelTime", qualifiedByName = "decimalToString")
     TicketDTO toDto(Ticket ticket);
 
-
     @Named("decimalToString")
-    public static String decimalToString(BigDecimal travelTime) {
+    static String decimalToString(BigDecimal travelTime) {
 
         int hours = travelTime.intValue() / 60;
         int minutes = travelTime.intValue() % 60;
@@ -32,13 +30,12 @@ public interface TicketMapper {
     }
 
     @Named("departureTimeMapping")
-    public static String departureTimeMapping(String departureDate){
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    static String departureTimeMapping(String departureDate) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-M-d");
         LocalDate date = LocalDate.parse(departureDate, formatter);
         formatter = DateTimeFormatter.ofPattern("d.MM, E", new Locale("uk"));
         return date.format(formatter);
     }
-
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     Ticket partialUpdate(TicketDTO ticketDTO, @MappingTarget Ticket ticket);
