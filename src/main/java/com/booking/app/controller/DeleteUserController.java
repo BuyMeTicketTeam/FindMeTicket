19dfe37400060790271b98f57fda422f5ccba7b1
Mykhailo Marchuk <mishaakamichael999@gmail.com>
2024-02-06 05:17:05 +0200

    Delete api for DELETE method to delete user and all of related info about him

--- src/main/java/com/booking/app/controller/DeleteUserController.java
+++ src/main/java/com/booking/app/controller/DeleteUserController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.NoSuchElementException;
 import java.util.UUID;
 
 import static com.booking.app.constant.CustomHttpHeaders.REMEMBER_ME;
@@ -26,6 +27,14 @@ public class DeleteUserController implements DeleteUserAPI {
 
     private final UserCredentialsRepository userCredentialsRepository;
 
+    /**
+     * Deletes a user and related information (cookies, etc.).
+     *
+     * @param request  The HttpServletRequest containing the HTTP request information.
+     * @param response The HttpServletResponse for sending the HTTP response.
+     * @return A ResponseEntity indicating a successful deletion of the user and related information.
+     * @throws NoSuchElementException If the USER_ID cookie is not present in the request.
+     */
     @DeleteMapping("/delete-user")
     public ResponseEntity<?> deleteUser(HttpServletRequest request, HttpServletResponse response) {
         String userId = CookieUtils.getCookie(request, USER_ID).orElseThrow().getValue();
@@ -33,7 +42,7 @@ public class DeleteUserController implements DeleteUserAPI {
         CookieUtils.deleteCookie(request, response, REFRESH_TOKEN);
         CookieUtils.deleteCookie(request, response, USER_ID);
         CookieUtils.deleteCookie(request, response, REMEMBER_ME);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(response);
     }
 
 }
