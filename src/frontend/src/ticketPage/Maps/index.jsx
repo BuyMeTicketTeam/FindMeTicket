bd91271b2f594faf8fcd247bc3b708f8dcbd5f53
Stepan Stadniuk <stepanigcom@gmail.com>
2024-02-16 22:05:54 +0200

    +map styles and move api key to env

--- src/frontend/src/ticketPage/Maps/index.jsx
+++ src/frontend/src/ticketPage/Maps/index.jsx
@@ -2,7 +2,7 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, { useRef, useState, useCallback } from 'react';
-import './style.css';
+import './style.scss';
 import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 import { useTranslation } from 'react-i18next';
 import TouristPlaces from './image 8.png';
@@ -48,7 +48,7 @@ function Maps() {
   function updateMarker(place) {
     const svgMarker = {
       path: 'M-1.547 12l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM0 0q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z',
-      fillColor: 'blue',
+      fillColor: '#355982',
       fillOpacity: 1,
       strokeWeight: 0,
       rotation: 0,
--- src/frontend/src/ticketPage/index.jsx
+++ src/frontend/src/ticketPage/index.jsx
@@ -7,11 +7,17 @@ import Price from './Price/index';
 import Information from './Information/index ';
 import Loader from '../Loader/index';
 import Error from '../Error';
+import Maps from './Maps';
 import './style.scss';
 
 function TicketPage() {
   const { ticketId } = useParams();
-  const [ticketData, setTicketData] = useState(null);
+  const [ticketData, setTicketData] = useState({
+    departureTime: 'asdads',
+    travelTime: 'dafsdf',
+    arrivalTime: 'asdasd',
+    departureCity: 'asdasd',
+  });
   const [ticketUrl, setTicketUrl] = useState([]);
   const [ticketError, setTicketError] = useState(false);
   const [connection, setConnection] = useState(true);
@@ -69,6 +75,7 @@ function TicketPage() {
       <Information ticketData={ticketData} />
       <div className="ticketPage-text">{t('price')}</div>
       <Price ticketUrls={ticketUrl} connection={connection} />
+      <Maps />
     </>
   );
   const ticketErrorView = ticketError && <Error />;
