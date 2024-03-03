2ae893fcaa999c3dd08a3021b2471fd102ada410
Mykhailo Marchuk <mishaakamichael999@gmail.com>
2024-03-03 23:01:49 +0200

    Custom async executor
    Signed-off-by: Mykhailo Marchuk <mishaakamichael999@gmail.com>

---
+++ src/main/java/com/booking/app/config/AsyncConfig.java
@@ -0,0 +1,27 @@
+package com.booking.app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig implements AsyncConfigurer {
+
+    @Override
+    @Bean(name = "asyncExecutor")
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(50);
+        executor.setMaxPoolSize(200);
+        executor.setQueueCapacity(40);
+        executor.setThreadNamePrefix("custom-async-");
+        executor.initialize();
+        return executor;
+    }
+
+}
