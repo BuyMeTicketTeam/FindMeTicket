4d7c74408db207b5825af0ee3cc7c78d665112f0
Mykhailo Marchuk <mishaakamichael999@gmail.com>
2024-03-29 20:51:16 +0200

    Find user by id from UserCredentials but not from User
    Signed-off-by: Mykhailo Marchuk <mishaakamichael999@gmail.com>

--- src/main/java/com/booking/app/services/impl/NotificationServiceImpl.java
+++ src/main/java/com/booking/app/services/impl/NotificationServiceImpl.java
@@ -1,6 +1,6 @@
 package com.booking.app.services.impl;
 
-import com.booking.app.repositories.UserRepository;
+import com.booking.app.repositories.UserCredentialsRepository;
 import com.booking.app.services.NotificationService;
 import jakarta.servlet.http.Cookie;
 import lombok.RequiredArgsConstructor;
@@ -13,15 +13,15 @@ import java.util.UUID;
 @Service
 @RequiredArgsConstructor
 public class NotificationServiceImpl implements NotificationService {
-    private final UserRepository userRepository;
+    private final UserCredentialsRepository userCredentialsRepository;
 
     @Override
     public void disable(Cookie id) {
         try {
             UUID userId = UUID.fromString(id.getValue());
-            userRepository.findById(userId).ifPresent(u -> {
-                u.setNotification(false);
-                userRepository.save(u);
+            userCredentialsRepository.findById(userId).ifPresent(u -> {
+                u.getUser().setNotification(false);
+                userCredentialsRepository.save(u);
             });
         } catch (IllegalArgumentException e) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, String.format("No user by %s id", id), e);
@@ -32,9 +32,9 @@ public class NotificationServiceImpl implements NotificationService {
     public void enable(Cookie id) {
         try {
             UUID userId = UUID.fromString(id.getValue());
-            userRepository.findById(userId).ifPresent(u -> {
-                u.setNotification(true);
-                userRepository.save(u);
+            userCredentialsRepository.findById(userId).ifPresent(u -> {
+                u.getUser().setNotification(true);
+                userCredentialsRepository.save(u);
             });
         } catch (IllegalArgumentException e) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, String.format("No user by %s id", id), e);
