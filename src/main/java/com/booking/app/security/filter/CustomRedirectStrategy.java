6129a60022568fabf8236d1128aa5dd6e09e594d
Mykhailo Marchuk <mishaakamichael999@gmail.com>
2024-04-02 11:38:59 +0300

    Refactor JwtAuthFilter and logic for logout redirection
    Signed-off-by: Mykhailo Marchuk <mishaakamichael999@gmail.com>

---
+++ src/main/java/com/booking/app/security/filter/CustomRedirectStrategy.java
@@ -0,0 +1,9 @@
+package com.booking.app.security.filter;
+
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomRedirectStrategy extends DefaultRedirectStrategy {
+
+}
