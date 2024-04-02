c5edf36d5d85f9b78b1dbaf4b43a36ba05317d45
KirilllP <penyazevkirill@gmail.com>
2024-04-02 12:50:24 +0300

    fixed incorrect city names in SearchHistoryDto.

--- src/main/java/com/booking/app/services/impl/SearchHistoryServiceImpl.java
+++ src/main/java/com/booking/app/services/impl/SearchHistoryServiceImpl.java
@@ -2,11 +2,13 @@ package com.booking.app.services.impl;
 
 import com.booking.app.dto.RequestTicketsDTO;
 import com.booking.app.dto.SearchHistoryDto;
+import com.booking.app.dto.StartLettersDTO;
 import com.booking.app.entity.User;
 import com.booking.app.entity.UserCredentials;
 import com.booking.app.entity.UserSearchHistory;
 import com.booking.app.mapper.HistoryMapper;
 import com.booking.app.repositories.SearchHistoryRepository;
+import com.booking.app.repositories.UkrPlacesRepository;
 import com.booking.app.repositories.UserCredentialsRepository;
 import com.booking.app.repositories.UserRepository;
 import com.booking.app.services.SearchHistoryService;
@@ -15,9 +17,11 @@ import com.booking.app.util.CookieUtils;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -39,6 +43,8 @@ public class SearchHistoryServiceImpl implements SearchHistoryService {
 
     private final HistoryMapper historyMapper;
 
+    private final UkrPlacesRepository ukrPlacesRepository;
+
     public void addToHistory(RequestTicketsDTO requestTicketsDTO, String language, HttpServletRequest request) {
         Optional<UUID> userId = getIdFromRequest(request);
 
@@ -55,8 +61,24 @@ public class SearchHistoryServiceImpl implements SearchHistoryService {
     public List<SearchHistoryDto> getUserHistory(HttpServletRequest request) {
         return getIdFromRequest(request)
                 .flatMap(userRepository::findById)
-                .map(user -> user.getUser().getHistory().stream().map(historyMapper::historyToDto).collect(Collectors.toList()))
+                .map(user -> user.getUser().getHistory().stream().map(t -> historyToDto(t, request.getHeader(HttpHeaders.CONTENT_LANGUAGE))).collect(Collectors.toList()))
                 .orElseThrow(() -> new UsernameNotFoundException("No such user"));
+    }
+
+    private SearchHistoryDto historyToDto(UserSearchHistory userSearchHistory, String language) {
+        String cityTo = getCity(userSearchHistory.getArrivalCityId(), language);
+        String cityFrom = getCity(userSearchHistory.getArrivalCityId(), language);
+
+        return SearchHistoryDto.builder()
+                .addingTime(userSearchHistory.getAddingTime().format(DateTimeFormatter.ofPattern("hh:mm, dd/MM/yyyy")))
+                .departureDate(userSearchHistory.getDepartureDate())
+                .arrivalCity(cityTo)
+                .departureCity(cityFrom).build();
+    }
+
+    private String getCity(Long id, String language) {
+        return language.equals("eng") ? ukrPlacesRepository.findById(id).get().getNameEng()
+                : ukrPlacesRepository.findById(id).get().getNameEng();
     }
 
     private Optional<UUID> getIdFromRequest(HttpServletRequest request) {
@@ -69,5 +91,6 @@ public class SearchHistoryServiceImpl implements SearchHistoryService {
             }
         }).orElse(Optional.empty());
     }
+
 
 }
