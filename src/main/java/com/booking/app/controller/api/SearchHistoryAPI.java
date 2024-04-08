445326e5b5edfcf2917aa92ee29646106798c93b
Mykhailo Marchuk <mishaakamichael999@gmail.com>
2024-04-08 14:57:13 +0300

    Refactors, User history dates format (adding time with pm/am), cities localization, List of history reversed
    Signed-off-by: Mykhailo Marchuk <mishaakamichael999@gmail.com>

--- src/main/java/com/booking/app/controller/api/SearchHistoryAPI.java
+++ src/main/java/com/booking/app/controller/api/SearchHistoryAPI.java
@@ -1,14 +1,16 @@
 package com.booking.app.controller.api;
 
+import com.booking.app.entity.UserCredentials;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 
-public interface SearchHistoryAPI {
+public interface SearchHistoryApi {
 
     @Operation(summary = "Searching history for user", description = "find search history by user id")
     @ApiResponse(responseCode = "200", description = "Returns history")
     @ApiResponse(responseCode = "404", description = "History not found")
-    public ResponseEntity<?> getHistory(HttpServletRequest request);
+    public ResponseEntity<?> getHistory(@AuthenticationPrincipal UserCredentials userCredentials, HttpServletRequest request);
 }
--- src/main/java/com/booking/app/services/impl/SearchHistoryServiceImpl.java
+++ src/main/java/com/booking/app/services/impl/SearchHistoryServiceImpl.java
@@ -2,31 +2,25 @@ package com.booking.app.services.impl;
 
 import com.booking.app.dto.RequestTicketsDTO;
 import com.booking.app.dto.SearchHistoryDto;
-import com.booking.app.dto.StartLettersDTO;
-import com.booking.app.entity.User;
+import com.booking.app.entity.UkrainianPlaces;
 import com.booking.app.entity.UserCredentials;
 import com.booking.app.entity.UserSearchHistory;
 import com.booking.app.mapper.HistoryMapper;
+import com.booking.app.mapper.model.ArrivalCity;
+import com.booking.app.mapper.model.DepartureCity;
 import com.booking.app.repositories.SearchHistoryRepository;
 import com.booking.app.repositories.UkrPlacesRepository;
 import com.booking.app.repositories.UserCredentialsRepository;
-import com.booking.app.repositories.UserRepository;
 import com.booking.app.services.SearchHistoryService;
 import com.booking.app.services.TypeAheadService;
 import com.booking.app.util.CookieUtils;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static com.booking.app.constant.CustomHttpHeaders.USER_ID;
@@ -45,10 +39,12 @@ public class SearchHistoryServiceImpl implements SearchHistoryService {
 
     private final UkrPlacesRepository ukrPlacesRepository;
 
-    public void addToHistory(RequestTicketsDTO requestTicketsDTO, String language, HttpServletRequest request) {
-        Optional<UUID> userId = getIdFromRequest(request);
 
-        Optional<UserCredentials> user = userId.flatMap(userRepository::findById);
+    @Override
+    public void addToHistory(RequestTicketsDTO requestTicketsDTO, String language, HttpServletRequest request) {
+        Optional<UUID> uuid = CookieUtils.getCookie(request, USER_ID).map(cookie -> UUID.fromString(cookie.getValue()));
+
+        Optional<UserCredentials> user = uuid.flatMap(userRepository::findById);
         user.ifPresent(t -> historyRepository.save(UserSearchHistory.builder()
                 .user(t.getUser())
                 .departureCityId(typeAheadService.getCityId(requestTicketsDTO.getDepartureCity(), language))
@@ -57,40 +53,22 @@ public class SearchHistoryServiceImpl implements SearchHistoryService {
                 .build()));
     }
 
-
-    public List<SearchHistoryDto> getUserHistory(HttpServletRequest request) {
-        return getIdFromRequest(request)
-                .flatMap(userRepository::findById)
-                .map(user -> user.getUser().getHistory().stream().map(t -> historyToDto(t, request.getHeader(HttpHeaders.CONTENT_LANGUAGE))).collect(Collectors.toList()))
-                .orElseThrow(() -> new UsernameNotFoundException("No such user"));
-    }
-
-    private SearchHistoryDto historyToDto(UserSearchHistory userSearchHistory, String language) {
-        String cityTo = getCity(userSearchHistory.getArrivalCityId(), language);
-        String cityFrom = getCity(userSearchHistory.getDepartureCityId(), language);
-
-        return SearchHistoryDto.builder()
-                .addingTime(userSearchHistory.getAddingTime().format(DateTimeFormatter.ofPattern("hh:mm, dd/MM/yyyy")))
-                .departureDate(userSearchHistory.getDepartureDate())
-                .arrivalCity(cityTo)
-                .departureCity(cityFrom).build();
+    @Override
+    public List<SearchHistoryDto> getUserHistory(UserCredentials userCredentials, String language) {
+        return Stream.of(userCredentials)
+                .flatMap(user -> user.getUser().getHistory().stream())
+                .map(history -> {
+                    String departureCity = getCity(history.getArrivalCityId(), language);
+                    String arrivalCity = getCity(history.getDepartureCityId(), language);
+                    return historyMapper.historyToDto(history, new DepartureCity(arrivalCity), new ArrivalCity(departureCity));
+                })
+                .toList().reversed();
     }
 
     private String getCity(Long id, String language) {
-        return language.equals("eng") ? ukrPlacesRepository.findById(id).get().getNameEng()
-                : ukrPlacesRepository.findById(id).get().getNameEng();
+        Optional<String> city = language.equals("eng") ? ukrPlacesRepository.findById(id).map(UkrainianPlaces::getNameEng)
+                : ukrPlacesRepository.findById(id).map(UkrainianPlaces::getNameUa);
+        return city.orElse(null);
     }
-
-    private Optional<UUID> getIdFromRequest(HttpServletRequest request) {
-        Optional<Cookie> cookie = CookieUtils.getCookie(request, USER_ID);
-        return cookie.map(c -> {
-            try {
-                return Optional.of(UUID.fromString(c.getValue()));
-            } catch (IllegalArgumentException e) {
-                return Optional.<UUID>empty();
-            }
-        }).orElse(Optional.empty());
-    }
-
 
 }
