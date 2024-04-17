f4f33c9d15ca5630016dbed4aaa192dc1ccb913c
maks2708 <148962309+maks2708@users.noreply.github.com>
2024-04-17 16:00:58 +0300

    Buyt 32 privacy policy (#84)
    * Privacy policy is ready
    
    * File changes
    
    * Bug fixed
    
    ---------
    
    Co-authored-by: Stepan Stadniuk <109167846+Stepan22-prog@users.noreply.github.com>

--- frontend/src/routers/index.jsx
+++ frontend/src/routers/index.jsx
@@ -12,14 +12,15 @@ import TicketPage from '../ticketPage';
 import Login from '../header/login/index';
 import ResetPassword from '../resetPassword';
 import ChangePassword from '../changePassword';
-import PrivacyPolicy from '../privacyPolicy';
+import PrivacyPolicyEng from '../privacyPolicy/index-eng';
+import PrivacyPolicyUa from '../privacyPolicy/index-ua';
 import TouristPlaces from '../TouristPlaces';
 import ProfilePage from '../newProfile';
 
 export default function Routers({
   updateAuthValue, ticketsData,
   setTicketsData, selectedTransport,
-  setSelectedTransport, auth,
+  setSelectedTransport, auth, language,
 }) {
   return (
     <Routes>
@@ -91,7 +92,12 @@ export default function Routers({
           </RouteController>
       )}
       />
-      <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+      {language.value === 'UA' && (
+        <Route path="/privacy-policy" element={<PrivacyPolicyUa />} />
+      )}
+      {language.value === 'ENG' && (
+        <Route path="/privacy-policy" element={<PrivacyPolicyEng />} />
+      )}
       <Route path="/tourist-places/:city?" element={<TouristPlaces auth={auth} />} />
       <Route
         path="/profile-page"
