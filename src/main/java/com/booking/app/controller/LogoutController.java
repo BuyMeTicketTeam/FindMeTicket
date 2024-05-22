aa8537695384161e46ce6c32808b7ba93a9838a5
Mykhailo Marchuk <mishaakamichael999@gmail.com>
2024-05-22 20:42:38 +0300

    Refactor LogoutController
    Signed-off-by: Mykhailo Marchuk <mishaakamichael999@gmail.com>

--- src/main/java/com/booking/app/controller/LogoutController.java
+++ src/main/java/com/booking/app/controller/LogoutController.java
@@ -1,7 +1,9 @@
 package com.booking.app.controller;
 
-import com.booking.app.controller.api.LogoutAPI;
 import com.booking.app.util.CookieUtils;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -20,21 +22,21 @@ import static com.booking.app.constant.JwtTokenConstants.REFRESH_TOKEN;
 @RestController
 @RequestMapping
 @RequiredArgsConstructor
-public class LogoutController implements LogoutAPI {
+@Tag(name = "Log out", description = "Log out authenticated user")
+public class LogoutController {
 
     /**
-     * Handles HTTP GET request to '/logout'.
-     * Clears the security context and invalidates the user's authentication.
+     * Handles HTTP GET request to '/logout'
      *
      * @return ResponseEntity with an HTTP status representing successful logout
      */
-    @Override
     @GetMapping("/logout")
+    @Operation(summary = "Log out a user")
+    @ApiResponse(responseCode = "200", description = "Successfully logged")
     public ResponseEntity<?> logout(HttpServletRequest request, HttpServletResponse response) {
         CookieUtils.deleteCookie(request, response, REFRESH_TOKEN);
         CookieUtils.deleteCookie(request, response, USER_ID);
         CookieUtils.deleteCookie(request, response, REMEMBER_ME);
-
         return ResponseEntity.ok().build();
     }
 
