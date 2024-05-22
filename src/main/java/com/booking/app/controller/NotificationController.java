06814e151dd2582b91f71ec23ffdbee0bedf5121
Mykhailo Marchuk <mishaakamichael999@gmail.com>
2024-05-22 20:45:28 +0300

    Refactor NotificationController
    Signed-off-by: Mykhailo Marchuk <mishaakamichael999@gmail.com>

--- src/main/java/com/booking/app/controller/NotificationController.java
+++ src/main/java/com/booking/app/controller/NotificationController.java
@@ -1,8 +1,11 @@
 package com.booking.app.controller;
 
-import com.booking.app.controller.api.NotificationApi;
 import com.booking.app.entity.UserCredentials;
 import com.booking.app.services.NotificationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
@@ -14,13 +17,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @Log4j2
-public class NotificationController implements NotificationApi {
+@Tag(name = "Notification", description = "Notifications management")
+public class NotificationController {
 
     private final NotificationService notificationService;
 
     @GetMapping("/notifications/disable")
     @PreAuthorize("#{hasAnyRole('USER', 'ADMIN')}")
-    @Override
+    @Operation(summary = "Disable")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Off"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized")
+    })
     public ResponseEntity<?> disableNotification(@AuthenticationPrincipal UserCredentials userCredentials) {
         notificationService.disable(userCredentials.getUser());
         return ResponseEntity.ok().build();
@@ -28,7 +36,11 @@ public class NotificationController implements NotificationApi {
 
     @GetMapping("/notifications/enable")
     @PreAuthorize("#{hasAnyRole('USER', 'ADMIN')}")
-    @Override
+    @Operation(summary = "Enable")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "On"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized")
+    })
     public ResponseEntity<?> enableNotification(@AuthenticationPrincipal UserCredentials userCredentials) {
         notificationService.enable(userCredentials.getUser());
         return ResponseEntity.ok().build();
