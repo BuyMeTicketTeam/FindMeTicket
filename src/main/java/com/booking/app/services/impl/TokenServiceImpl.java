5c41e93111c7a6576616f38aea2eff8e905a56a1
Mykhailo Marchuk <mishaakamichael999@gmail.com>
2024-05-24 02:08:53 +0300

    Refactor TokenServiceImpl
    Signed-off-by: Mykhailo Marchuk <mishaakamichael999@gmail.com>

--- src/main/java/com/booking/app/services/impl/TokenServiceImpl.java
+++ src/main/java/com/booking/app/services/impl/TokenServiceImpl.java
@@ -1,6 +1,6 @@
 package com.booking.app.services.impl;
 
-import com.booking.app.entity.UserCredentials;
+import com.booking.app.entity.User;
 import com.booking.app.repositories.UserCredentialsRepository;
 import com.booking.app.services.TokenService;
 import lombok.RequiredArgsConstructor;
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
-import java.util.Optional;
 
 /**
  * Service class for handling confirmation tokens and user verification.
@@ -20,29 +19,15 @@ public class TokenServiceImpl implements TokenService {
 
     private final UserCredentialsRepository userCredentialsRepository;
 
-    /**
-     * Verifies a given token for a specific user.
-     *
-     * @param email      The email associated with the user.
-     * @param givenToken The token to be verified.
-     * @return boolean Returns true if the token is valid and not expired, false otherwise.
-     */
     @Override
     public boolean verifyToken(String email, String givenToken) {
-        Optional<UserCredentials> userByEmail = userCredentialsRepository.findByEmail(email);
         LocalDateTime now = LocalDateTime.now();
         Date dateExpiryTime = Date.from(now.atZone(ZoneId.systemDefault()).toInstant());
-
-        if (userByEmail.isPresent()) {
-            String token = userByEmail.get().getUser().getConfirmToken().getToken();
-            if (dateExpiryTime.before(userByEmail.get().getUser().getConfirmToken().getExpiryTime())) {
-                if (token.equals(givenToken)) {
-                    return true;
-                }
-            }
-        }
-
-        return false;
+        return userCredentialsRepository.findByEmail(email).map(userCredentials -> {
+            User user = userCredentials.getUser();
+            String token = user.getConfirmToken().getToken();
+            return dateExpiryTime.before(user.getConfirmToken().getExpiryTime()) && token.equals(givenToken);
+        }).orElse(false);
     }
 
 }
