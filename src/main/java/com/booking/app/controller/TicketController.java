a5620b8b06faac2f8438be250b7b1c1280aaa494
Mykhailo Marchuk <mishaakamichael999@gmail.com>
2024-05-24 03:57:05 +0300

    Refactor TicketController
    Signed-off-by: Mykhailo Marchuk <mishaakamichael999@gmail.com>

--- src/main/java/com/booking/app/controller/TicketController.java
+++ src/main/java/com/booking/app/controller/TicketController.java
@@ -1,6 +1,5 @@
 package com.booking.app.controller;
 
-import com.booking.app.controller.api.TicketApi;
 import com.booking.app.dto.RequestSortedTicketsDTO;
 import com.booking.app.dto.RequestTicketsDTO;
 import com.booking.app.dto.TicketDto;
@@ -9,8 +8,14 @@ import com.booking.app.services.SearchHistoryService;
 import com.booking.app.services.SortTicketsService;
 import com.booking.app.services.TicketService;
 import com.booking.app.services.impl.scrape.ScraperManager;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -29,7 +34,8 @@ import java.util.concurrent.CompletableFuture;
 @RestController
 @RequestMapping(produces = {MediaType.TEXT_EVENT_STREAM_VALUE})
 @RequiredArgsConstructor
-public class TicketController implements TicketApi {
+@Tag(name = "Ticket web scraping", description = "Endpoints for scraping tickets' info")
+public class TicketController {
 
     private final ScraperManager scrapingService;
 
@@ -40,47 +46,74 @@ public class TicketController implements TicketApi {
     private final SearchHistoryService searchHistoryService;
 
     @PostMapping("/searchTickets")
-    @Override
-    public ResponseBodyEmitter findTickets(@RequestBody RequestTicketsDTO ticketsDTO, @RequestHeader(HttpHeaders.CONTENT_LANGUAGE) String siteLanguage, HttpServletResponse response, HttpServletRequest request) throws IOException, ParseException {
-
+    @Operation(summary = "Search tickets", description = "Find tickets based by criteria")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Tickets has been found"),
+            @ApiResponse(responseCode = "404", description = "Tickets is not found")
+    })
+    public ResponseBodyEmitter findTickets(@RequestBody @NotNull @Valid RequestTicketsDTO ticketsDTO,
+                                           @RequestHeader(HttpHeaders.CONTENT_LANGUAGE) String siteLanguage,
+                                           HttpServletResponse response,
+                                           HttpServletRequest request) throws IOException, ParseException {
         validateLanguage(siteLanguage);
-
         searchHistoryService.addHistory(ticketsDTO, siteLanguage, request);
 
         SseEmitter emitter = new SseEmitter();
-
         CompletableFuture<Boolean> isTicketScraped = scrapingService.findTickets(ticketsDTO, emitter, siteLanguage);
 
         isTicketScraped.thenAccept(isFound -> {
-            if (Boolean.TRUE.equals(isFound)) response.setStatus(HttpStatus.OK.value());
-            else response.setStatus(HttpStatus.NOT_FOUND.value());
+            if (Boolean.TRUE.equals(isFound)) {
+                response.setStatus(HttpStatus.OK.value());
+            } else {
+                response.setStatus(HttpStatus.NOT_FOUND.value());
+            }
         });
         return emitter;
     }
 
     @GetMapping("/get/ticket/{id}")
-    @Override
-    public ResponseBodyEmitter getTicketById(@PathVariable("id") UUID id, @RequestHeader(HttpHeaders.CONTENT_LANGUAGE) String siteLanguage, HttpServletResponse response) throws IOException, ParseException {
+    @Operation(summary = "Single ticket", description = "Ticket by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ticket has been found"),
+            @ApiResponse(responseCode = "404", description = "Ticket is not found")
+    })
+    public ResponseBodyEmitter getTicketById(@PathVariable("id") UUID id,
+                                             @RequestHeader(HttpHeaders.CONTENT_LANGUAGE) String siteLanguage,
+                                             HttpServletResponse response) throws IOException, ParseException {
         validateLanguage(siteLanguage);
+
         SseEmitter emitter = new SseEmitter();
         CompletableFuture<Boolean> isTicketFound = scrapingService.getTicket(id, emitter, siteLanguage);
 
-        isTicketFound.thenAccept(isFound -> response.setStatus(Boolean.TRUE.equals(isFound) ? HttpStatus.OK.value() : HttpStatus.NOT_FOUND.value()));
+        isTicketFound.thenAccept(isFound -> {
+            if (Boolean.TRUE.equals(isFound)) {
+                response.setStatus(HttpStatus.OK.value());
+            } else {
+                response.setStatus(HttpStatus.NOT_FOUND.value());
+            }
+        });
         return emitter;
     }
 
     @PostMapping(value = "/sortedBy", produces = MediaType.APPLICATION_JSON_VALUE)
-    @Override
-    public ResponseEntity<?> getSortedTickets(@RequestBody RequestSortedTicketsDTO requestSortedTicketsDTO, @RequestHeader(HttpHeaders.CONTENT_LANGUAGE) String siteLanguage, HttpServletRequest request) {
+    @Operation(summary = "Sort tickets", description = "Either by price, travel time, departure, or arrival")
+    @ApiResponse(responseCode = "200", description = "Get sorted tickets")
+    public ResponseEntity<?> getSortedTickets(@RequestBody @NotNull @Valid RequestSortedTicketsDTO requestSortedTicketsDTO,
+                                              @RequestHeader(HttpHeaders.CONTENT_LANGUAGE) String siteLanguage) {
         validateLanguage(siteLanguage);
         return ResponseEntity.ok().body(sortTicketsService.getSortedTickets(requestSortedTicketsDTO, siteLanguage));
     }
 
     @PostMapping(value = "/selectedTransport", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<TicketDto>> getSelectedTransportTicket(@RequestBody RequestTicketsDTO requestTicketsDTO) throws IOException {
+    @Operation(summary = "Get tickets", description = "Get tickets by type transport")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully returned"),
+            @ApiResponse(responseCode = "404", description = "No tickets of the requested type were found")
+    })
+    public ResponseEntity<List<TicketDto>> getSelectedTransportTicket(@RequestBody @NotNull @Valid RequestTicketsDTO requestTicketsDTO) throws IOException {
         return ticketService.getBusTickets(requestTicketsDTO)
                 .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     private void validateLanguage(String language) {
