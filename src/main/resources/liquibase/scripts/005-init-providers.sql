ab88607d6b01524bef733ea152ef995dcc734d19
Mykhailo Marchuk <mishaakamichael999@gmail.com>
2024-08-07 19:28:08 +0300

    Update sql scripts
    Signed-off-by: Mykhailo Marchuk <mishaakamichael999@gmail.com>

--- src/main/resources/liquibase/scripts/005-init-providers.sql
+++
@@ -1,6 +0,0 @@
-CREATE TYPE provider_type AS ENUM ('BASIC', 'GOOGLE', 'FACEBOOK');
-
-INSERT INTO find_me_ticket_schema.provider (id, type)
-VALUES (1, 'BASIC'),
-       (2, 'GOOGLE'),
-       (3, 'FACEBOOK');
